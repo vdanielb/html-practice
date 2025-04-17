f509926e8cb4d2d4f58db671c4b6eafc80a0e1c2
Daniel Budidharma <dharma.daniel2803@gmail.com>
2025-04-16 20:58:40 -0700

    added js automation

---
+++ global.js
@@ -0,0 +1,43 @@
+console.log('IT’S ALIVE!');
+
+function $$(selector, context = document) {
+  return Array.from(context.querySelectorAll(selector));
+}
+
+let pages = [
+  { url: '', title: 'Home' },
+  { url: 'contact/', title: 'Contact' },
+  { url: 'projects/', title: 'Projects' },
+  { url: 'https://github.com/vdanielb', title: 'GitHub' },
+  { url: 'resume/', title: 'Resume' },
+];
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
+  ? "/"                  // Local server
+  : "/html-practice/";         // GitHub Pages repo name
+
+for (let p of pages) {
+  let url = p.url;
+  let title = p.title;
+  if (!url.startsWith('http')) {
+    url = BASE_PATH + url;
+  }
+  nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
+}
+
+
+
+// let navLinks = $$("nav a");
+
+// let currentLink = navLinks.find(
+//   (a) => a.host === location.host && a.pathname === location.pathname,
+// );
+
+// if (currentLink) {
+//   currentLink.classList.add('current');
+// } else {
+//   console.log("No current link found.");
+// }
